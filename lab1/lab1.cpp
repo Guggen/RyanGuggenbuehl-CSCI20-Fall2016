cd790a48346cc35ecabb0cc24d74924df41f5fe5
Ryan Guggenbuehl <rguggenbuehl001@student.butte.edu>
2016-08-25 20:54:11 +0000

    ASCII Art Lab 1

---
+++ lab1/lab1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"=============="<<endl;
+    cout<<"=====----====="<<endl;
+    cout<<"===.-...-..==="<<endl;
+    cout<<"==..O..O....=="<<endl;
+    cout<<"=.....L......="<<endl;
+    cout<<"==..____/...=="<<endl;
+    cout<<"=====----====="<<endl;
+    cout<<"=============="<<endl;
+}
+
+
+
+
