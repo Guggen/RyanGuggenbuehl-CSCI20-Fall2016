4e3a965ae1cc196d149257b981e000fafbe2c7ff
Ryan Guggenbuehl <rguggenbuehl001@student.butte.edu>
2016-08-30 20:25:12 +0000

    Lab2 submission

---
+++ lab2/lab2.cpp
@@ -0,0 +1,6 @@
+//1.the computer chooses a number between 1 and 20
+//2.user guesses number between 1 and 10
+//3.the computer then responds by saying if number is higher or lower than chosen number
+//4.the user then guess another number either higher or lower than the first guessed number
+//5. steps 3 and 4 are repeated before the number is either guess correctly and wins or the user loses by running out of turns.
+//6. the user starts with 10 points and each incorrect guess a point is removed until they hit 0 and the game is over.
