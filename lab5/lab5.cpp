ba43c732909b00a94d4acf14609b8ecbb6715d09
Ryan Guggenbuehl <rguggenbuehl001@student.butte.edu>
2016-09-07 21:11:44 +0000

    Lab5

--- lab5/lab5.cpp
+++ lab5/lab5.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-struct Monster{
+struct MonsterOne{
     string name;
     string head;
     string eyes;
@@ -12,29 +12,78 @@ struct Monster{
     
 };
 
+struct MonsterTwo{
+    string name;
+    string head;
+    string eyes;
+    string ears;
+    string nose;
+    string mouth;
+    
+};
+
+struct MonsterThree{
+    string name;
+    string head;
+    string eyes;
+    string ears;
+    string nose;
+    string mouth;
+    
+};
+
+
 int main () {
-    Monster features;
+    MonsterOne monster1;
+    
+    
+    MonsterTwo monster2;
+    
+    monster2.name = "PickleJuice";
+    monster2.head = "Happy";
+    monster2.eyes = "Vegitas";
+    monster2.ears = "Spritem";
+    monster2.nose = "Vegitas";
+    monster2.mouth = "Spritem";
+    
+    
+    MonsterThree monster3;
+    
+    monster3.name = "Zumba";
+    monster3.head = "Franken";
+    monster3.eyes = "Wackus";
+    monster3.ears = "Spritem";
+    monster3.nose = "Spritem";
+    monster3.mouth = "Wackus";
     
     cout << "Enter Name: ";
-    getline(cin, features.name); 
+    cin >> monster1.name; 
     
     cout << "Enter head: ";
-    cin >> features.head;
+    cin >> monster1.head;
     
     cout << "Enter eyes: ";
-    cin >> features.eyes;
+    cin >> monster1.eyes;
     
     cout << "Enter ears: ";
-    cin >> features.ears;
+    cin >> monster1.ears;
     
     cout << "Enter nose: ";
-    cin >> features.nose;
+    cin >> monster1.nose;
     
     cout << "Enter mouth: ";
-    cin >> features.mouth;
+    cin >> monster1.mouth;
     
-    cout << features.name << ": "<< features.head << ", " 
-    << features.eyes << ", " << features.ears << ", "
-    << features.nose << ", " << features.mouth<<"."<< endl;
+    cout <<endl<<endl<< monster1.name << ": "<< monster1.head << ", " 
+    << monster1.eyes << ", " << monster1.ears << ", "
+    << monster1.nose << ", " << monster1.mouth<<"."<< endl<< endl;
+    
+    cout << monster2.name << ": "<< monster2.head << ", " 
+    << monster2.eyes << ", " << monster2.ears << ", "
+    << monster2.nose << ", " << monster2.mouth<<"."<< endl<<endl;
+    
+    cout << monster3.name << ": "<< monster3.head << ", " 
+    << monster3.eyes << ", " << monster3.ears << ", "
+    << monster3.nose << ", " << monster3.mouth<<"."<< endl<<endl;
     
 }
