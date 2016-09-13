d81a0a6c889ce173d986111143ce47905a54d388
Ryan Guggenbuehl <rguggenbuehl001@student.butte.edu>
2016-09-13 20:43:26 +0000

    Lab6

---
+++ lab6/lab6.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+
+    
+    string name = "Ryan Guggenbuehl ";
+    string year = " 2016";
+    string copyright = "©";
+    
+void Face (){    
+    cout<<"=============="<<endl
+        <<"=====----====="<<endl
+        <<"===.-...-..==="<<endl
+        <<"==..O..O....=="<<endl
+        <<"=.....L......="<<endl
+        <<"==..____/...=="<<endl
+        <<"=====----====="<<endl
+        <<"=============="<<endl << endl
+        << name << copyright << year <<endl;
+        
+        
+        return;
+}
+
+int main () {
+    Face();
+    return 0;
+
+}
