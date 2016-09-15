9e6706b24c74847e6fde6cf29d6add9600b3879d
Ryan Guggenbuehl <rguggenbuehl001@student.butte.edu>
2016-09-15 21:11:15 +0000

    Lab7

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -3,14 +3,18 @@
 #include <math.h>
 using namespace std;
 
-void song(string animal, string sound){
+void Chorus1(string animal){
 
     cout<< "Old MacDonald had a farm,"<< endl
         << "E-I-E-I-O." << endl << endl
         <<"And on his farm he had some " << animal << ","<< endl
-        << "E-I-E-I-O." << endl << endl
-        << "With a " << sound<<", "<< sound<< " here," << endl
-        << "And a " << sound<< ", " << sound <<"there," << endl
+        << "E-I-E-I-O." << endl << endl;
+        
+}
+
+void Chorus2(string sound){
+    cout<< "With a " << sound<<", "<< sound<< " here," << endl
+        << "And a " << sound<< ", " << sound <<" there," << endl
         << "Here a " << sound << ", there a " << sound <<","<< endl
         << "Everywhere a " << sound << ", " << sound << "," << endl << endl
         << "Old MacDonald had a farm, " << endl
@@ -19,15 +23,24 @@ void song(string animal, string sound){
 
 int main(){
     
-    song("chicks","chick");
-    song("duck","quack");
-    song("turkey","gobble");
-    song("pig","oink");
-    song("cow","moo");
-    song("cat","meow");
-    song("mule","heehaw");
-    song("dog","bow wow");
-    song("turtle","nerp, nerp");
+    Chorus1("chicks");
+    Chorus2("chick");
+    Chorus1("duck");
+    Chorus2("quack");
+    Chorus1("turkey");
+    Chorus2("gobble");
+    Chorus1("pig");
+    Chorus2("oink");
+    Chorus1("cow");
+    Chorus2("moo");
+    Chorus1("cat");
+    Chorus2("meow");
+    Chorus1("mule");
+    Chorus2("heehaw");
+    Chorus1("dog");
+    Chorus2("bow wow");
+    Chorus1("turtle");
+    Chorus2("nerp, nerp");
 }
 
 
