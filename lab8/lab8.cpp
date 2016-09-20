f8b5e2e332de2b1bf6b61ba06f5d2ae2bd788c64
Ryan Guggenbuehl <rguggenbuehl001@student.butte.edu>
2016-09-20 21:03:40 +0000

    Lab 8

--- lab8/lab8.cpp
+++ lab8/lab8.cpp
@@ -3,14 +3,14 @@
 #include <math.h>
 using namespace std;
 
-double ToKilo(double pounds)
+double ToKilo(double pounds)//Pounds to kilo function
 {
     double kilograms = 0.0;
     kilograms = pounds / 2.2046226218;
     return kilograms;
 }
 
-double ToPounds(double kilograms)
+double ToPounds(double kilograms)//Kilograms to pounds function
 {
     double pounds = 0.0;
     pounds = kilograms * 2.2046226218;
@@ -25,18 +25,18 @@ int main()
     double number = 0.0;
     double number2 = 0.0;
     
-    cout << "Enter Kilograms: " << endl;
+    cout << "Enter Kilograms: " << endl;//Kilograms to pounds
     cin >> kilograms;
     ToPounds(kilograms);
     number2 = ToPounds(kilograms);
-    cout << number2 << "lb" << endl;
+    cout << "Converted " << kilograms <<"kg to "<< number2 << "lbs." << endl;
     
     
     
-    cout << "Enter pounds: " << endl;
+    cout << "Enter pounds: " << endl;//Pounds to kilo
     cin >> pounds;
     ToKilo(pounds);
     number = ToKilo(pounds);
-    cout << number << "kg" << endl;
+    cout << "Converted " << pounds <<"lbs to "<< number << "kgs." << endl;
     
 }
