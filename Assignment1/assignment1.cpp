3bad743823b36eb0228e1c527cecf4bd03f3ced1
Ryan Guggenbuehl <rguggenbuehl001@student.butte.edu>
2016-10-02 01:10:14 +0000

    Assignment 1

--- Assignment1/assignment1.cpp
+++ Assignment1/assignment1.cpp
@@ -1,4 +1,5 @@
-//Ryan Guggenbuehl
+//Created by: Ryan Guggenbuehl 
+//Created on: 9/16/16
 //Income program
 
 #include <iostream>
@@ -6,7 +7,7 @@
 using namespace std;
 
 int main(){ 
-    string name = "";
+    string name = "";//Variable deffinitions
     float hrWage = 0;
     int weeklyHrs = 0;
     int salary = 0;
@@ -14,19 +15,19 @@ int main(){
     int months = 12;
     float taxedSalary = 0;
     
-    cout << "Enter name: ";
+    cout << "Enter name: ";//Persons name
     getline(cin, name);
     
-    cout << "Enter hourly wage: ";
+    cout << "Enter hourly wage: ";//Persons Wage 
     cin >> hrWage;
     
-    cout << "Enter weekly hours: ";
+    cout << "Enter weekly hours: ";//Persons hours
     cin >> weeklyHrs;
     
-    salary = hrWage * weeklyHrs * weeks * months;
+    salary = hrWage * weeklyHrs * weeks * months;//Salary equation
     taxedSalary = salary -salary*0.17;
     
-    cout << "Name: " << name <<". " << "Weekly Hours: " << weeklyHrs << ". " 
+    cout << "Name: " << name <<". " << "Weekly Hours: " << weeklyHrs << ". " //Viewable Output
     << "Hourly Wage: $" << hrWage << ". "<< "Gross pay: $" << salary << ". "<< "Net pay: $" 
     << taxedSalary << ".";
 }
