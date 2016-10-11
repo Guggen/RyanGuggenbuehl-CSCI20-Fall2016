90de8c33fd9d76f778c44fa792e48e7663659622
Ryan Guggenbuehl <rguggenbuehl001@student.butte.edu>
2016-10-11 21:03:30 +0000

    Lab 13

--- lab12/lab12.cpp
+++ lab12/lab12.cpp
@@ -1,6 +1,7 @@
 //Created by Ryan Guggenbuehl
 //Created on 9/10/16
 //Getting to know you
+//Questions that procede through nested if else statements
 
 
 
@@ -19,42 +20,42 @@ int main(){
     string answer4 = "";
     
     
-    cout << "Do you like the outdoors? yes/no:"<< endl;
+    cout << "Do you like the outdoors? yes/no:"<< endl;//First question
     cin >> answer1;
     
     if(answer1 == "yes"){
-        cout << "Do you like to run? yes/no: " << endl;
+        cout << "Do you like to run? yes/no: " << endl;//second question if answerd yes
         cin >> answer2;
     
         if (answer2 == "yes" ){
-            cout << "You are a horse!" << endl;
+            cout << "You are a horse!" << endl;//Animal if answerd yes
         }
        
         else{ 
-            cout << "You are a turtle!"<< endl;
+            cout << "You are a turtle!"<< endl;//Animal if answerd no
         }
     }
     
     
     else{ 
-        cout << "Do you like cheese?" << endl;
+        cout << "Do you like cheese?" << endl;//second question if answerd no
         cin >> answer3;
         
         
         if( answer3 == "yes"){
-            cout << "You are a mouse!" << endl;
+            cout << "You are a mouse!" << endl;//Third animal if answerd yes
         }
         
         
         else{ 
-            cout << "Do you like to swim?"<< endl;
+            cout << "Do you like to swim?"<< endl;//Third question if answerd no
             cin >> answer4;
             if (answer4 == "yes"){
-                cout << "You are a fish!" << endl;
+                cout << "You are a fish!" << endl;//Third answer if yes
             
             }
             else { 
-                cout << "You are a cat!" << endl;
+                cout << "You are a cat!" << endl;//Third answer if no
             }
         }
     }
